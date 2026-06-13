f78bab3521639d769eb27c985d9b1d860b1af7e3
MadPierceee <grantgilvillareal2002@gmail.com>
2026-06-13 17:44:14 +0800

    Sitemap and Robot improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-// NOTE: keep `site` in sync with SITE_URL in src/config/site.ts.
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.carolinafinefoods.com',
+  site: 'https://www.cffsimpsonville.com',
   integrations: [react(), sitemap()],
 });
